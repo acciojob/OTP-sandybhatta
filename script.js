5c70d517bc9323a05db0d448d61a81e3de0b847c
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-07-02 17:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 //your JS code here. If required.
-
-const codes = document.querySelectorAll(".code");
-codes[0].focus();
-codes.forEach((val,ind){
+  let codes = document.querySelectorAll(".code");
+            codes[0].focus();
+        codes.forEach((val,ind)=>{
 	
-	val.addEventListener("input",(e)=>{
-		if(e.target.value.length>0 && ind<codes.length-1){
-			codes[ind+1].focus();
-		}
-	})
-	val.addEventListener("keydown",(e)=>{
-		if(e.key=="Backspace"){
-			e.target.value="";
-			if(ind>0){
-				codes[ind-1].focus();
-			}
-		}
-	})
-})
+	        val.addEventListener("input",(e)=>{
+		        if(e.target.value.length>0 && ind<codes.length-1){
+			        codes[ind+1].focus();
+		        }
+	        })
+	        val.addEventListener("keydown",(e)=>{
+		        if(e.key=="Backspace" &&ind>0){
+			        codes[ind-1].focus();
+		        }
+	        })
+        })
